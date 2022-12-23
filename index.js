c0089fd425ce3a1da269e38d7abb6b64be4b6299
Dmitriy Herasymenko <shiningxors@gmail.com>
2022-12-23 09:05:21 +0200

    add hedler close dropdown to click global window

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const status = document.querySelector(".status");
 
 const dropdow = (callback) => {
   dropdowns.forEach((dropdown) => {
+   
     const select = dropdown.querySelector(".select");
     const caret = dropdown.querySelector(".caret");
     const menu = dropdown.querySelector(".menu");
     const options = dropdown.querySelectorAll(".menu li");
     const selected = dropdown.querySelector(".selected");
 
-    select.addEventListener("click", () => {
+
+    select.addEventListener("click", (e) => {
       select.classList.toggle("select-clicked");
       caret.classList.toggle("caret-rotate");
       menu.classList.toggle("menu-open");
@@ -22,7 +24,6 @@ const dropdow = (callback) => {
         select.classList.remove("select-clicked");
         caret.classList.remove("caret-rotate");
         menu.classList.remove("menu-open");
-
         if (callback) {
           callback(e);
         }
@@ -30,10 +31,18 @@ const dropdow = (callback) => {
         options.forEach((option) => {
           option.classList.remove("active");
         });
+
+       
         option.classList.add("active");
       });
     });
   });
 };
 
-dropdow((e) => console.log(e));
+dropdow((e) => console.log("e", e));
+document.body.addEventListener("click", function(e) {
+  if(e.target === this) {
+    const menu = document.querySelector(".menu-open");
+    menu.classList.remove("menu-open");
+  }
+})
